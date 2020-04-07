0b216f2bddbfc62ba5f34bc26b3b48cde0f2b38b
Paolo <p.fiaschi@hotmail.it>
2020-04-07 22:14:00 +0200

    cambio colore del contatto selezionato

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
 
 
-
+  // dichiarazione variabili
   var invio = $(".fa-telegram-plane");
   var msgInviati = $(".conversazione");
   var scrivoMsg = $(".invio input");
   var opzioniMsg = $(".msgInviati");
+  var msgRicevuti = $(".conversazione");
+  var contattoAnteprima = $(".contattoAnteprima");
 
+  //visualizzo solo il microfono e non il pulsante invio
   scrivoMsg.click(function(){
     $(".microfono").css('display', 'none');
     $(".invioIcon").css('display', 'block');
 
   });
+
   // centralizzo la funzione per inviare msg al click
   function inviaMsg(){
     var contenutoMsg = $(".invio input").val();
     msgInviati.append("<div class='msgInviati'>" + '<p class="msgInviatiTesto">' + contenutoMsg + '</p>' + "</div>");
+    // //risposta automatica del pc
+    // setTimeout(inviaMsg, 1000);
+    // function inviaMsg(){
+    //   console.log("ok");
+    //   msgRicevuti.append("<div class='msgRicevuti'>" + '<p class="msgRicevutiTesto">' + "ok" + '</p>' + "</div>");
+    // }
+    //
 
     // msgInviati.append("<div class='msgInviati'>" + '<p class="msgInviatiTesto">'+ "<span>" +  contenutoMsg  + "</span>" + "<span class='opzioniMsg'" + "<i class='fas fa-chevron-down'>" + "</i>" + "</span>" + '</p>' + "</div>");
     console.log($(this));
@@ -25,18 +36,27 @@ $(document).ready(function(){
 
   }
   //aggiungere la freccia in basso per il menu a tendina
+
   // opzioniMsg.mouseover(function(){
   //   $(".opzioniMsg").addClass('.opzioniMsgActive');
   // });
+
+
   // invia il msg con tasto invio (tasto 13)
   $(".invio input").keydown(function(event) {
 
     console.log(event);
-    /* Act on the event */
     if (event.which == 13) {
       console.log("invio");
       var contenutoMsg = $(".invio input").val();
       msgInviati.append("<div class='msgInviati'>" + '<p class="msgInviatiTesto">' + contenutoMsg + '</p>' + "</div>");
+      // //risposta automatica del pc
+
+      // setTimeout(inviaMsg, 1000);
+      // function inviaMsg(){
+      //   console.log("ok");
+      //   msgRicevuti.append("<div class='msgRicevuti'>" + '<p class="msgRicevutiTesto">' + "ok" + '</p>' + "</div>");
+      // }
       $(".invio input").val("");
       $(".microfono").css('display', 'block');
       $(".invioIcon").css('display', 'none');
@@ -45,7 +65,13 @@ $(document).ready(function(){
   // invia il msg al click
   invio.click(inviaMsg);
 
+  //Al click cambia colore il contatto selezionato
+  contattoAnteprima.click(function(){
+    contattoAnteprima.removeClass('contattoAnteprimaActive');
+    $(this).addClass('contattoAnteprimaActive');
 
+
+  });
 
 
 
