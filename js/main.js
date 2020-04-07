c979fce3b96ebcfe16192ef74a9a75190fd4efa5
Paolo <p.fiaschi@hotmail.it>
2020-04-07 18:14:11 +0200

    aggiunta parte "invio" js e css

--- js/main.js
+++ js/main.js
@@ -1,18 +1,46 @@
 $(document).ready(function(){
 
 
-    var invio = $(".fa-telegram-plane");
-    var contenutoMsg = $(".invio input").val();
-    var msgInviati = $(".msgInviati ul");
-    var inputValdopoClick;
 
-    // invia il msg al click
-    invio.click(function(){
+      var invio = $(".fa-telegram-plane");
+      var msgInviati = $(".msgInviati ul");
+      var scrivoMsg = $(".invio input");
 
-      msgInviati.append('<li>' + contenutoMsg + '</li>');
-      inputValdopoClick = $(".invio input").val("");
+
+
+
+      scrivoMsg.click(function(){
+        $(".microfono").css('display', 'none');
+        $(".invioIcon").css('display', 'block');
+
+      });
+      // centralizzo la funzione per inviare msg al click
+      function inviaMsg(){
+        var contenutoMsg = $(".invio input").val();
+        msgInviati.append("<ul>" + '<li>' + contenutoMsg + '</li>' + "</ul>" );
+        $(".invio input").val("");
+        $(".microfono").css('display', 'block');
+        $(".invioIcon").css('display', 'none');
+
       }
-    );
+
+      $(".invio input").keydown(function(event) {
+
+        console.log(event);
+        /* Act on the event */
+        if (event.which == 13) {
+          console.log("invio");
+          var contenutoMsg = $(".invio input").val();
+          msgInviati.append('<li>' + contenutoMsg + '</li>');
+          $(".invio input").val("");
+          $(".microfono").css('display', 'block');
+          $(".invioIcon").css('display', 'none');
+        }
+      });
+      // invia il msg al click
+      invio.click(inviaMsg);
+
+      // invia il msg con tasto invio (tasto 13)
 
 
 
